11d137801dde47d24c711ff493fb14d60f14f6d8
iSergium <isergium@gmail.com>
2025-04-28 04:02:11 +0500

    clear

--- html/popup.js
+++ html/popup.js
@@ -1,7 +1,5 @@
-const CLASS_THEME_USED = 'used',
-      CLASS_HAS_UNREAD = 'hasUnread',
-      CLASS_LOADING = 'loading',
-      CLASS_HIDDEN = 'hidden';
+const CLASS_THEME_USED = 'used';
+const CLASS_ACCENT = 'accent';
 
 let elements = {};
 
@@ -23,16 +21,16 @@ window.onload = () => {
             elements.favoritesBox.textContent = response.favorites.count;
             elements.mentionsBox.textContent = response.mentions.count;
 
-            if (response.qms.count) elements.qmsBox.classList.add(CLASS_HAS_UNREAD);
-            if (response.mentions.count) elements.mentionsBox.classList.add(CLASS_HAS_UNREAD);
+            if (response.qms.count) elements.qmsBox.classList.add(CLASS_ACCENT);
+            if (response.mentions.count) elements.mentionsBox.classList.add(CLASS_ACCENT);
 
             if (response.favorites.count) {
-                elements.favoritesBox.classList.add(CLASS_HAS_UNREAD);
+                elements.favoritesBox.classList.add(CLASS_ACCENT);
                 for (let theme of response.favorites.list) {
                     add_theme_row(theme);
                 }
             } else {
-                let tpl = document.getElementById('tpl_no_themes').cloneNode(true);
+                let tpl = document.getElementById('tpl-no-themes').content.cloneNode(true);
                 elements.themesList.appendChild(tpl);
             }
         }
@@ -41,56 +39,56 @@ window.onload = () => {
 
 
 function init() {
-    elements.username_label = document.getElementById('panelUsername');
+    elements.username_label = document.getElementById('user-name');
     elements.username_label.addEventListener("click", () => {
         chrome.runtime.sendMessage({action: 'open_url', what: 'user'});
     });
 
-    elements.qmsBox = document.getElementById('panelQMS');
+    elements.qmsBox = document.getElementById('header-qms');
     elements.qmsBox.addEventListener("click", () => {
         chrome.runtime.sendMessage({action: 'open_url', what: 'qms'});
     });
 
-    elements.favoritesBox = document.getElementById('panelFavorites');
+    elements.favoritesBox = document.getElementById('header-favorites');
     elements.favoritesBox.addEventListener("click", () => {
         chrome.runtime.sendMessage({action: 'open_url', what: 'favorites'});
     });
 
-    elements.mentionsBox = document.getElementById('panelMentions');
+    elements.mentionsBox = document.getElementById('header-mentions');
     elements.mentionsBox.addEventListener("click", () => {
         chrome.runtime.sendMessage({action: 'open_url', what: 'mentions'});
     });
     //
-    elements.themesList = document.getElementById('themesList');
-    elements.themeTPL = document.getElementById('tpl_one_theme');
+    elements.themesList = document.getElementById('topic-list');
+    elements.themeTPL = document.getElementById('tpl-one-topic');
 }
 
 /**
  * @param {FavoriteTheme} theme 
  */
 function add_theme_row(theme) {
-    let tpl = elements.themeTPL.cloneNode(true),
-        tpl_caption = tpl.querySelector('.oneTheme_caption'),
-        tpl_last_user = tpl.querySelector('.oneTheme_user'),
-        tpl_last_dt = tpl.querySelector('.oneTheme_lastPost');
+    let tpl = elements.themeTPL.content.cloneNode(true),
+        tpl_li = tpl.querySelector('li'),
+        tpl_caption = tpl.querySelector('.topic-title'),
+        tpl_last_user = tpl.querySelector('.user'),
+        tpl_last_dt = tpl.querySelector('.date');
     
     tpl_caption.textContent = theme.title;
     tpl_last_user.textContent = theme.last_user_name;
     tpl_last_dt.textContent = new Date(theme.last_post_ts*1000).toLocaleString();
 
-    if (theme.pin) tpl_caption.classList.add('oneTheme_pin');
-    tpl.addEventListener('click', (el) => {
+    if (theme.pin) tpl_caption.classList.add(CLASS_ACCENT);
+    tpl_li.addEventListener('click', (el) => {
         let current = el.target;
         //current.classList.add(CLASS_LOADING);
-        //console.log(el, current);
-        if (current.classList.contains('lastPost')) {
+        if (current.classList.contains('tli')) {
             chrome.runtime.sendMessage({
                 action: 'open_url',
                 what: 'favorites',
                 id: theme.id,
                 view: 'getlastpost'
             }).then(() => {
-                tpl.classList.add(CLASS_THEME_USED);
+                tpl_li.classList.add(CLASS_THEME_USED);
             });
         } else {
             chrome.runtime.sendMessage({
@@ -98,7 +96,7 @@ function add_theme_row(theme) {
                 what: 'favorites',
                 id: theme.id
             }).then(() => {
-                tpl.classList.add(CLASS_THEME_USED);
+                tpl_li.classList.add(CLASS_THEME_USED);
             });
         }
     });
